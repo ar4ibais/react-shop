be66651e8a9a2ab89628046878592dd6c6801f0e
ar4ibais <ak.hutov@yandex.ru>
2023-10-15 15:44:47 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Footer from "./components/Footer/Footer"
+import Header from "./components/Header/Header"
+import Main from "./components/Main/Main"
+
+
+import './styles/common.css';
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,7 @@
+const Main = () => {
+    return (
+        <main className="main container">Shop</main>
+    );
+}
+
+export default Main;
